11aabf4a217d6c038eecf76c082d1c0533e2af04
Sid <shawsss953@gmail.com>
2024-12-27 15:56:14 +0530

    second commit

---
+++ EvenOdd.java
@@ -0,0 +1,31 @@
+package com.wipro.day1;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("Enter a number");
+		int x= sc.nextInt();
+		int y=9;
+		//int result= ++x +y + x++;;
+		System.out.println(++x);
+		System.out.println(x++);
+		//System.out.println(result);
+//		if(x % 2 == 0)
+//		{
+//			System.out.println(x + " is even");
+//		}
+//		else
+//		{
+//			System.out.println(x + " is odd");
+//		}
+		
+		
+	}
+
+}
